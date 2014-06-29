1208149f156a21337d157ae62d1c00f27455ee84
Victor Grigoriu <vgrigoriu@gmail.com>
2014-06-29 23:24:40 +0300

    Parse an int

---
+++ src/FromString.Tests/ParsedTTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace FromString.Tests
+{
+    public class ParsedTTests
+    {
+        [Fact]
+        public void CanParseAnInt()
+        {
+            var parsedInt = new Parsed<int>("123");
+
+            Assert.True(parsedInt.HasValue);
+            Assert.Equal(123, parsedInt.Value);
+        }
+    }
+}
---
+++ src/FromString/Parsed{T}.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FromString
+{
+    public delegate bool TryParse<T>(string rawValue, out T result);
+
+    public class Parsed<T>
+    {
+
+        private readonly bool hasValue;
+        private readonly T value;
+
+        public Parsed(string rawValue)
+        {
+            var parser = (TryParse<T>)ParsedHelper.Parsers[typeof(T)];
+            hasValue = parser(rawValue, out value);
+        }
+
+        public bool HasValue
+        {
+            get { return hasValue; }
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+    }
+
+    internal static class ParsedHelper
+    {
+        /// <summary>
+        /// The dictionary is in a separate class because, in Parsed&lt;T>,
+        /// there would have been one dictionary for each type parameter
+        /// </summary>
+        public static readonly Dictionary<Type, Delegate> Parsers = new Dictionary<Type, Delegate>();
+
+        static ParsedHelper()
+        {
+            AddParser<int>(int.TryParse);
+        }
+
+        public static void AddParser<T>(TryParse<T> parser)
+        {
+            Parsers[typeof (T)] = parser;
+        }
+    }
+}

e2e9a71f18b69e45b29b1ddc4c162d85af2fb023
Victor Grigoriu <vgrigoriu@gmail.com>
2014-07-03 11:48:08 +0300

    Create nuspec

--- src/FromString/Properties/AssemblyInfo.cs
+++ src/FromString/Properties/AssemblyInfo.cs
@@ -1,15 +1,13 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FromString")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple library that tries to parse everything")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Teamnet")]
 [assembly: AssemblyProduct("FromString")]
 [assembly: AssemblyCopyright("Copyright © 2014 Teamnet")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
